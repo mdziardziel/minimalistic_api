814e1955b4ed5975365e5afa30f07775fe25d5b0
mdziardziel <mdziardziel@gmail.com>
2019-11-23 21:44:09 +0100

    update /

--- index.js
+++ index.js
@@ -3,18 +3,6 @@ const sqlite3 = require('sqlite3').verbose();
 
 var express = require("express"), app = express(), port = 3070;
 
-app.get("/", function(req, res) {
-    let db = new sqlite3.Database('./db/chinook.db');
-    let sql = `SELECT DISTINCT Name name FROM playlists ORDER BY name`;
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-          throw err;
-        }
-        res.json(rows);
-      });
-    db.close();
-})
-
 app.use("/api", require("./routes/routes"));
 
 app.get("*", function(req, res) {
